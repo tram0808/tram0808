be7035b8c616d18aa6abfe8a9eb34258ca865f38
tram0808 <phanquynhbichtram88@gmail.com>
2020-04-03 17:34:42 +0700

    update, addword o dau

--- src/common/List.js
+++ src/common/List.js
@@ -71,16 +71,11 @@ import {Text,View,StyleSheet,TouchableOpacity,FlatList,TextInput,} from 'react-n
           vn: this.txtVn,
           isMemorized: false,
         };
-        const newWords = this.state.words.concat(newWord);
+        const newWords = Object.assign([], this.state.words);
+        newWords.splice(0, 0, newWord);
         this.txtVn = '';
         this.txtEn = '';
-        this.setState({words: newWords, shouldShowform: false}, () => {
-          const length = this.state.words.length;
-          this.flatlist.scrollToIndex({
-            animated: true,
-            index: length - 2 + '',
-          });
-        });
+       this,this.setState({words: newWords, shouldShowform: false});
       };
     renderForm = () => {
       if (this.state.shouldShowform) {
